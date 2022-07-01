f0a9f734b40a2b1152f1db44bead65e26e6f8cc7
Dan Calise <calise@gmail.com>
2022-07-01 15:00:18 -0400

    attempting submit

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,10 @@ import * as yup from "yup";
 const labelClass = `block text-gray-700 text-sm font-bold mb-2`;
 const inputClass = `shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`;
 
-const config = {
-  cors: "https://cors-anywhere.herokuapp.com/", // <optional> doesn't display the cors error
-  formUrl:
-    "https://docs.google.com/forms/d/e/1AmwjvAKpXB68Jc4PkPI5CZUe6SrgUGfsifGUDEguFxA/formResponse"
-};
-
 const schema = yup
   .object({
+    gm: yup.string().required("Please enter your name"),
+    team: yup.string().required("Please enter your team name"),
     players: yup.array().of(
       yup.object().shape({
         name: yup
@@ -41,10 +37,12 @@ const schema = yup
   .required();
 
 type Inputs = {
+  gm: string;
+  team: string;
   players: {
     name: string;
-    aav: number;
-    years: number;
+    aav: number | null;
+    years: number | null;
   }[];
 };
 
@@ -57,7 +55,8 @@ export default function App() {
   } = useForm<Inputs>({
     resolver: yupResolver(schema),
     defaultValues: {
-      players: [{ name: "", aav: 0, years: 0 }]
+      team: "",
+      players: [{ name: "", aav: null, years: null }]
     }
   });
 
@@ -65,17 +64,104 @@ export default function App() {
     control,
     name: "players"
   });
-  console.log(fields.length);
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    const payload = data.players.map((player) => ({
+      gm: data.gm,
+      team: data.team,
+      playerName: player.name,
+      aav: player.aav,
+      years: player.years
+    }));
+
+    console.log("payload", payload);
+    try {
+      const res = await fetch(
+        "https://script.google.com/macros/s/AKfycbxxS48j0q54V5rLzyFzzmvM0uqNVJTT4_TfWcLEOwTncMU5olFBOec2ddjbYWi07do/exec",
+        { method: "POST", body: JSON.stringify(payload) }
+      );
+
+      console.log(res);
+    } catch (error) {
+      console.log("error!", error);
+    }
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-lg">
+      <div className="p-4">
+        <div className="flex flex-wrap -mx-3 mb-2">
+          <div className="w-full px-3">
+            <label htmlFor="name" className={labelClass}>
+              Name
+            </label>
+            <input id="name" className={inputClass} {...register(`gm`)} />
+
+            {errors?.gm && (
+              <span className="text-red-700">{errors?.gm?.message}</span>
+            )}
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-2">
+          <div className="w-full px-3">
+            <label htmlFor="name" className={labelClass}>
+              Team
+            </label>
+            <select id="name" className={inputClass} {...register(`team`)}>
+              <option disabled value="">
+                Please select your team
+              </option>
+              <option disabled value="">
+                Eastern Conference
+              </option>
+
+              <option value="BOS">Boston Bruins</option>
+              <option value="BUF">Buffalo Sabres</option>
+              <option value="CAR">Carolina Hurricanes</option>
+              <option value="CBJ">Columbus Blue Jackets</option>
+              <option value="DET">Detroit Red Wings</option>
+              <option value="FLA">Florida Panthers</option>
+              <option value="MON">Montreal Canadiens</option>
+              <option value="NJD">New Jersey Devils</option>
+              <option value="NYI">New York Islanders</option>
+              <option value="NYR">New York Rangers</option>
+              <option value="OTT">Ottawa Senators</option>
+              <option value="PHI">Philadelphia Flyers</option>
+              <option value="PIT">Pittsburgh Penguins</option>
+              <option value="TBL">Tampa Bay Lightning</option>
+              <option value="TOR">Toronto Maple Leafs</option>
+              <option value="WAS">Washington Capitals</option>
+              <option disabled value="">
+                Western Conference
+              </option>
+              <option value="ANA">Anaheim Ducks</option>
+              <option value="ARI">Arizona Coyotes</option>
+              <option value="CGY">Calgary Flames</option>
+              <option value="CHI">Chicago Blackhawks</option>
+              <option value="COL">Colorado Avalanche</option>
+              <option value="DAL">Dallas Stars</option>
+              <option value="EDMN">Edmonton Oilers</option>
+              <option value="LAK">Los Angeles Kings</option>
+              <option value="MIN">Minnesota Wild</option>
+              <option value="NSH">Nashville Predators</option>
+              <option value="SJS">San Jose Sharks</option>
+              <option value="SEA">Seattle Kraken</option>
+              <option value="STL">St Louis Blues</option>
+              <option value="VAN">Vancouver Canucks</option>
+              <option value="VGK">Vegas Golden Knights</option>
+              <option value="WPG">Winnipeg Jets</option>
+            </select>
+            {errors?.team && (
+              <span className="text-red-700">{errors?.team?.message}</span>
+            )}
+          </div>
+        </div>
+      </div>
       {fields.map((field, index) => (
         <div className="p-4">
           <div className="flex flex-wrap -mx-3 mb-2">
             <div className="w-full px-3">
               <label htmlFor="name" className={labelClass}>
-                Name
+                Player Name
               </label>
               <input
                 id="name"
@@ -84,7 +170,9 @@ export default function App() {
                 {...register(`players.${index}.name`)}
               />
               {errors?.players?.[index]?.name && (
-                <span>{errors?.players?.[index]?.name?.message}</span>
+                <span className="text-red-700">
+                  {errors?.players?.[index]?.name?.message}
+                </span>
               )}
             </div>
           </div>
@@ -101,7 +189,9 @@ export default function App() {
                 {...register(`players.${index}.aav`)}
               />
               {errors?.players?.[index]?.aav && (
-                <span>{errors.players[index].aav.message}</span>
+                <span className="text-red-700">
+                  {errors.players[index].aav.message}
+                </span>
               )}
             </div>
             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
@@ -115,7 +205,9 @@ export default function App() {
                 {...register(`players.${index}.years`)}
               />
               {errors?.players?.[index]?.years && (
-                <span>{errors.players[index].years.message}</span>
+                <span className="text-red-700">
+                  {errors.players[index].years.message}
+                </span>
               )}
             </div>
             {fields.length > 1 && (
@@ -137,8 +229,8 @@ export default function App() {
         onClick={() =>
           append({
             name: "",
-            aav: 0,
-            years: 0
+            aav: null,
+            years: null
           })
         }
       >
