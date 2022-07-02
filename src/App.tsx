c9f043673f09de9c8317b80ed433a257e26c0bcd
Dan Calise <calise@gmail.com>
2022-07-02 14:31:45 -0400

    populating dropdowns

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,13 @@ import {
 } from "./styles";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Salary } from "./Salary";
+import { batch } from "./batch";
 
 import { schema } from "./schema";
 import { Inputs } from "./types";
 import { supabase } from "./supabaseClient";
 import { useEffect, useState } from "react";
+import { FiChevronDown } from "react-icons/fi";
 
 export default function App() {
   const {
@@ -54,7 +56,6 @@ export default function App() {
 
   const [salaryTotal, setSalaryTotal] = useState(0);
   const players = watch("players");
-  console.log(players);
   useEffect(() => {
     let total = 0;
     players.forEach((player) => {
@@ -72,8 +73,8 @@ export default function App() {
         className="w-full mx-auto max-w-lg"
       >
         <div className="p-4">
-          <h1>Flex Hockey League</h1>
-          <h2>UFAX Batch 1</h2>
+          <h1 className="text-3xl">Flex Hockey League</h1>
+          <h2 className="text-xl">UFAX Batch 1</h2>
         </div>
         <div className="p-4">
           <div className="flex flex-wrap -mx-3 mb-2">
@@ -92,50 +93,57 @@ export default function App() {
               <label htmlFor="name" className={labelClass}>
                 Team
               </label>
-              <select id="name" className={inputClass} {...register(`team`)}>
-                <option disabled value="">
-                  Please select your team
-                </option>
-                <option disabled value="">
-                  Eastern Conference
-                </option>
+              <div className="mt-1 relative rounded-md shadow-sm">
+                <select id="name" className={inputClass} {...register(`team`)}>
+                  <option value="" disabled>
+                    Select your team
+                  </option>
+                  <option value="" disabled>
+                    Eastern Conference
+                  </option>
 
-                <option value="BOS">Boston Bruins</option>
-                <option value="BUF">Buffalo Sabres</option>
-                <option value="CAR">Carolina Hurricanes</option>
-                <option value="CBJ">Columbus Blue Jackets</option>
-                <option value="DET">Detroit Red Wings</option>
-                <option value="FLA">Florida Panthers</option>
-                <option value="MON">Montreal Canadiens</option>
-                <option value="NJD">New Jersey Devils</option>
-                <option value="NYI">New York Islanders</option>
-                <option value="NYR">New York Rangers</option>
-                <option value="OTT">Ottawa Senators</option>
-                <option value="PHI">Philadelphia Flyers</option>
-                <option value="PIT">Pittsburgh Penguins</option>
-                <option value="TBL">Tampa Bay Lightning</option>
-                <option value="TOR">Toronto Maple Leafs</option>
-                <option value="WAS">Washington Capitals</option>
-                <option disabled value="">
-                  Western Conference
-                </option>
-                <option value="ANA">Anaheim Ducks</option>
-                <option value="ARI">Arizona Coyotes</option>
-                <option value="CGY">Calgary Flames</option>
-                <option value="CHI">Chicago Blackhawks</option>
-                <option value="COL">Colorado Avalanche</option>
-                <option value="DAL">Dallas Stars</option>
-                <option value="EDMN">Edmonton Oilers</option>
-                <option value="LAK">Los Angeles Kings</option>
-                <option value="MIN">Minnesota Wild</option>
-                <option value="NSH">Nashville Predators</option>
-                <option value="SJS">San Jose Sharks</option>
-                <option value="SEA">Seattle Kraken</option>
-                <option value="STL">St Louis Blues</option>
-                <option value="VAN">Vancouver Canucks</option>
-                <option value="VGK">Vegas Golden Knights</option>
-                <option value="WPG">Winnipeg Jets</option>
-              </select>
+                  <option value="BOS">Boston Bruins</option>
+                  <option value="BUF">Buffalo Sabres</option>
+                  <option value="CAR">Carolina Hurricanes</option>
+                  <option value="CBJ">Columbus Blue Jackets</option>
+                  <option value="DET">Detroit Red Wings</option>
+                  <option value="FLA">Florida Panthers</option>
+                  <option value="MON">Montreal Canadiens</option>
+                  <option value="NJD">New Jersey Devils</option>
+                  <option value="NYI">New York Islanders</option>
+                  <option value="NYR">New York Rangers</option>
+                  <option value="OTT">Ottawa Senators</option>
+                  <option value="PHI">Philadelphia Flyers</option>
+                  <option value="PIT">Pittsburgh Penguins</option>
+                  <option value="TBL">Tampa Bay Lightning</option>
+                  <option value="TOR">Toronto Maple Leafs</option>
+                  <option value="WAS">Washington Capitals</option>
+                  <option disabled value="">
+                    Western Conference
+                  </option>
+                  <option value="ANA">Anaheim Ducks</option>
+                  <option value="ARI">Arizona Coyotes</option>
+                  <option value="CGY">Calgary Flames</option>
+                  <option value="CHI">Chicago Blackhawks</option>
+                  <option value="COL">Colorado Avalanche</option>
+                  <option value="DAL">Dallas Stars</option>
+                  <option value="EDMN">Edmonton Oilers</option>
+                  <option value="LAK">Los Angeles Kings</option>
+                  <option value="MIN">Minnesota Wild</option>
+                  <option value="NSH">Nashville Predators</option>
+                  <option value="SJS">San Jose Sharks</option>
+                  <option value="SEA">Seattle Kraken</option>
+                  <option value="STL">St Louis Blues</option>
+                  <option value="VAN">Vancouver Canucks</option>
+                  <option value="VGK">Vegas Golden Knights</option>
+                  <option value="WPG">Winnipeg Jets</option>
+                </select>
+                <div className="absolute inset-y-0 right-0 pr-4 flex items-center pointer-events-none">
+                  <span className="text-gray-500 sm:text-sm">
+                    <FiChevronDown className="text-xl" />
+                  </span>
+                </div>
+              </div>
               {errors?.team && (
                 <span className="text-red-700">{errors?.team?.message}</span>
               )}
@@ -149,12 +157,28 @@ export default function App() {
                 <label htmlFor="name" className={labelClass}>
                   Player Name
                 </label>
-                <input
-                  id="name"
-                  key={field.id}
-                  className={inputClass}
-                  {...register(`players.${index}.name`)}
-                />
+                <div className="mt-1 relative rounded-md shadow-sm">
+                  <select
+                    id="name"
+                    key={field.id}
+                    className={inputClass}
+                    {...register(`players.${index}.name`)}
+                  >
+                    <option value="" disabled>
+                      Select a player
+                    </option>
+                    {batch.map((player) => (
+                      <option
+                        value={player.name}
+                      >{`${player.pos} ${player.name} ${player.ov}ov`}</option>
+                    ))}
+                  </select>
+                  <div className="absolute inset-y-0 right-0 pr-4 flex items-center pointer-events-none">
+                    <span className="text-gray-500 sm:text-sm">
+                      <FiChevronDown className="text-xl" />
+                    </span>
+                  </div>
+                </div>
                 {errors?.players?.[index]?.name && (
                   <span className="text-red-700">
                     {errors?.players?.[index]?.name?.message}
@@ -164,7 +188,7 @@ export default function App() {
             </div>
 
             <div className="flex flex-wrap -mx-3 mb-2 justify-end">
-              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <div className="w-full md:w-1/2 px-3 mb-2 md:mb-0">
                 <label htmlFor="name" className={labelClass}>
                   AAV
                 </label>
@@ -180,7 +204,7 @@ export default function App() {
                   </span>
                 )}
               </div>
-              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <div className="w-full md:w-1/2 px-3 mb-2 md:mb-0">
                 <label htmlFor="years" className={labelClass}>
                   Years
                 </label>
--- src/schema.ts
+++ src/schema.ts
@@ -2,27 +2,23 @@ import * as yup from "yup";
 
 export const schema = yup
   .object({
-    gm: yup.string().required("Please enter your name"),
-    team: yup.string().required("Please enter your team name"),
+    gm: yup.string().required("Enter your name"),
+    team: yup
+      .string()
+      .required("Enter the name of the team you are bidding for"),
     players: yup.array().of(
       yup.object().shape({
-        name: yup
-          .string()
-          .required("Please enter the player's first and last name"),
+        name: yup.string().required("Select a player"),
         aav: yup
           .number()
-          .typeError(
-            "Please enter a number without any commas or special characters"
-          )
-          .required("Please enter a value between 1 and 3")
+          .typeError("Enter the contact AAV")
+          .required()
           .min(500000, "The minimum contract AAV is $500,000")
           .max(12500000, "This maximum contract AAV is $12,500,000"),
 
         years: yup
           .number()
-          .typeError(
-            "Please enter a number without any commas or special characters"
-          )
+          .typeError("Enter a number between 1 and 3")
           .required()
           .min(1, "The minimum contract length is 1 year.")
           .max(3, "This maximum contract length is 3 years.")
