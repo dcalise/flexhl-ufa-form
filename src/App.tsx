818e7c21b230a21d26cc078a551e1343beb44452
Dan Calise <calise@gmail.com>
2022-07-02 15:47:37 -0400

    type error

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,11 @@ import { Inputs } from "./types";
 import { supabase } from "./supabaseClient";
 import { useEffect, useState } from "react";
 import { FiChevronDown } from "react-icons/fi";
+import { PostgrestError } from "@supabase/supabase-js";
 
 export default function App() {
+  const [appState, setAppState] = useState("new");
+  const [appErrors, setAppErrors] = useState<PostgrestError | null>(null);
   const {
     register,
     control,
@@ -35,22 +38,22 @@ export default function App() {
     control,
     name: "players"
   });
-  const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    const payload = data.players.map((player) => ({
-      gm: data.gm,
-      team: data.team,
-      playerName: player.name,
+  const onSubmit: SubmitHandler<Inputs> = async (formData) => {
+    const payload = formData.players.map((player) => ({
+      gm: formData.gm,
+      team: formData.team,
+      playerrName: player.name,
       aav: player.aav,
       years: player.years
     }));
 
-    try {
-      const { data, error } = await supabase.from("ufax2022").insert(payload);
-
-      console.log("data", data);
-      console.log("error", error);
-    } catch (error) {
-      console.log(error);
+    const { data, error } = await supabase.from("ufax2022").insert(payload);
+    if (error) {
+      setAppErrors(error);
+      setAppState("failed");
+    }
+    if (data) {
+      setAppState("success");
     }
   };
 
@@ -68,110 +71,80 @@ export default function App() {
 
   return (
     <div>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="w-full mx-auto max-w-lg"
-      >
-        <div className="p-4">
-          <h1 className="text-3xl">Flex Hockey League</h1>
-          <h2 className="text-xl">UFAX Batch 1</h2>
-        </div>
-        <div className="p-4">
-          <div className="flex flex-wrap -mx-3 mb-2">
-            <div className="w-full px-3">
-              <label htmlFor="name" className={labelClass}>
-                Name
-              </label>
-              <input id="name" className={inputClass} {...register(`gm`)} />
-              {errors?.gm && (
-                <span className="text-red-700">{errors?.gm?.message}</span>
-              )}
-            </div>
+      {(appState === "new" || appState === "failed") && (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="w-full mx-auto max-w-lg py-10"
+        >
+          <div className="p-4">
+            <h1 className="text-3xl">Flex Hockey League</h1>
+            <h2 className="text-xl">UFAX Batch 1</h2>
           </div>
-          <div className="flex flex-wrap -mx-3 mb-2">
-            <div className="w-full px-3">
-              <label htmlFor="name" className={labelClass}>
-                Team
-              </label>
-              <div className="mt-1 relative rounded-md shadow-sm">
-                <select id="name" className={inputClass} {...register(`team`)}>
-                  <option value="" disabled>
-                    Select your team
-                  </option>
-                  <option value="" disabled>
-                    Eastern Conference
-                  </option>
-
-                  <option value="BOS">Boston Bruins</option>
-                  <option value="BUF">Buffalo Sabres</option>
-                  <option value="CAR">Carolina Hurricanes</option>
-                  <option value="CBJ">Columbus Blue Jackets</option>
-                  <option value="DET">Detroit Red Wings</option>
-                  <option value="FLA">Florida Panthers</option>
-                  <option value="MON">Montreal Canadiens</option>
-                  <option value="NJD">New Jersey Devils</option>
-                  <option value="NYI">New York Islanders</option>
-                  <option value="NYR">New York Rangers</option>
-                  <option value="OTT">Ottawa Senators</option>
-                  <option value="PHI">Philadelphia Flyers</option>
-                  <option value="PIT">Pittsburgh Penguins</option>
-                  <option value="TBL">Tampa Bay Lightning</option>
-                  <option value="TOR">Toronto Maple Leafs</option>
-                  <option value="WAS">Washington Capitals</option>
-                  <option disabled value="">
-                    Western Conference
-                  </option>
-                  <option value="ANA">Anaheim Ducks</option>
-                  <option value="ARI">Arizona Coyotes</option>
-                  <option value="CGY">Calgary Flames</option>
-                  <option value="CHI">Chicago Blackhawks</option>
-                  <option value="COL">Colorado Avalanche</option>
-                  <option value="DAL">Dallas Stars</option>
-                  <option value="EDMN">Edmonton Oilers</option>
-                  <option value="LAK">Los Angeles Kings</option>
-                  <option value="MIN">Minnesota Wild</option>
-                  <option value="NSH">Nashville Predators</option>
-                  <option value="SJS">San Jose Sharks</option>
-                  <option value="SEA">Seattle Kraken</option>
-                  <option value="STL">St Louis Blues</option>
-                  <option value="VAN">Vancouver Canucks</option>
-                  <option value="VGK">Vegas Golden Knights</option>
-                  <option value="WPG">Winnipeg Jets</option>
-                </select>
-                <div className="absolute inset-y-0 right-0 pr-4 flex items-center pointer-events-none">
-                  <span className="text-gray-500 sm:text-sm">
-                    <FiChevronDown className="text-xl" />
-                  </span>
-                </div>
-              </div>
-              {errors?.team && (
-                <span className="text-red-700">{errors?.team?.message}</span>
-              )}
-            </div>
-          </div>
-        </div>
-        {fields.map((field, index) => (
           <div className="p-4">
             <div className="flex flex-wrap -mx-3 mb-2">
               <div className="w-full px-3">
                 <label htmlFor="name" className={labelClass}>
-                  Player Name
+                  Name
+                </label>
+                <input id="name" className={inputClass} {...register(`gm`)} />
+                {errors?.gm && (
+                  <span className="text-red-700">{errors?.gm?.message}</span>
+                )}
+              </div>
+            </div>
+            <div className="flex flex-wrap -mx-3 mb-2">
+              <div className="w-full px-3">
+                <label htmlFor="name" className={labelClass}>
+                  Team
                 </label>
                 <div className="mt-1 relative rounded-md shadow-sm">
                   <select
                     id="name"
-                    key={field.id}
                     className={inputClass}
-                    {...register(`players.${index}.name`)}
+                    {...register(`team`)}
                   >
                     <option value="" disabled>
-                      Select a player
+                      Select your team
                     </option>
-                    {batch.map((player) => (
-                      <option
-                        value={player.name}
-                      >{`${player.pos} ${player.name} ${player.ov}ov`}</option>
-                    ))}
+                    <option value="" disabled>
+                      Eastern Conference
+                    </option>
+
+                    <option value="BOS">Boston Bruins</option>
+                    <option value="BUF">Buffalo Sabres</option>
+                    <option value="CAR">Carolina Hurricanes</option>
+                    <option value="CBJ">Columbus Blue Jackets</option>
+                    <option value="DET">Detroit Red Wings</option>
+                    <option value="FLA">Florida Panthers</option>
+                    <option value="MON">Montreal Canadiens</option>
+                    <option value="NJD">New Jersey Devils</option>
+                    <option value="NYI">New York Islanders</option>
+                    <option value="NYR">New York Rangers</option>
+                    <option value="OTT">Ottawa Senators</option>
+                    <option value="PHI">Philadelphia Flyers</option>
+                    <option value="PIT">Pittsburgh Penguins</option>
+                    <option value="TBL">Tampa Bay Lightning</option>
+                    <option value="TOR">Toronto Maple Leafs</option>
+                    <option value="WAS">Washington Capitals</option>
+                    <option disabled value="">
+                      Western Conference
+                    </option>
+                    <option value="ANA">Anaheim Ducks</option>
+                    <option value="ARI">Arizona Coyotes</option>
+                    <option value="CGY">Calgary Flames</option>
+                    <option value="CHI">Chicago Blackhawks</option>
+                    <option value="COL">Colorado Avalanche</option>
+                    <option value="DAL">Dallas Stars</option>
+                    <option value="EDMN">Edmonton Oilers</option>
+                    <option value="LAK">Los Angeles Kings</option>
+                    <option value="MIN">Minnesota Wild</option>
+                    <option value="NSH">Nashville Predators</option>
+                    <option value="SJS">San Jose Sharks</option>
+                    <option value="SEA">Seattle Kraken</option>
+                    <option value="STL">St Louis Blues</option>
+                    <option value="VAN">Vancouver Canucks</option>
+                    <option value="VGK">Vegas Golden Knights</option>
+                    <option value="WPG">Winnipeg Jets</option>
                   </select>
                   <div className="absolute inset-y-0 right-0 pr-4 flex items-center pointer-events-none">
                     <span className="text-gray-500 sm:text-sm">
@@ -179,91 +152,154 @@ export default function App() {
                     </span>
                   </div>
                 </div>
-                {errors?.players?.[index]?.name && (
-                  <span className="text-red-700">
-                    {errors?.players?.[index]?.name?.message}
-                  </span>
+                {errors?.team && (
+                  <span className="text-red-700">{errors?.team?.message}</span>
                 )}
               </div>
             </div>
+          </div>
+          {fields.map((field, index) => (
+            <div className="p-4">
+              <div className="flex flex-wrap -mx-3 mb-2">
+                <div className="w-full px-3">
+                  <label htmlFor="name" className={labelClass}>
+                    Player Name
+                  </label>
+                  <div className="mt-1 relative rounded-md shadow-sm">
+                    <select
+                      id="name"
+                      key={field.id}
+                      className={inputClass}
+                      {...register(`players.${index}.name`)}
+                    >
+                      <option value="" disabled>
+                        Select a player
+                      </option>
+                      {batch.map((player) => (
+                        <option
+                          value={player.name}
+                        >{`${player.pos} ${player.name} ${player.ov}ov`}</option>
+                      ))}
+                    </select>
+                    <div className="absolute inset-y-0 right-0 pr-4 flex items-center pointer-events-none">
+                      <span className="text-gray-500 sm:text-sm">
+                        <FiChevronDown className="text-xl" />
+                      </span>
+                    </div>
+                  </div>
+                  {errors?.players?.[index]?.name && (
+                    <span className="text-red-700">
+                      {errors?.players?.[index]?.name?.message}
+                    </span>
+                  )}
+                </div>
+              </div>
 
-            <div className="flex flex-wrap -mx-3 mb-2 justify-end">
-              <div className="w-full md:w-1/2 px-3 mb-2 md:mb-0">
-                <label htmlFor="name" className={labelClass}>
-                  AAV
-                </label>
+              <div className="flex flex-wrap -mx-3 mb-2 justify-end">
+                <div className="w-full md:w-1/2 px-3 mb-2 md:mb-0">
+                  <label htmlFor="name" className={labelClass}>
+                    AAV
+                  </label>
 
-                <Salary
-                  control={control}
-                  {...register(`players.${index}.aav`)}
-                />
+                  <Salary
+                    control={control}
+                    {...register(`players.${index}.aav`)}
+                  />
 
-                {errors?.players?.[index]?.aav && (
-                  <span className="text-red-700">
-                    {errors?.players?.[index]?.aav?.message}
-                  </span>
+                  {errors?.players?.[index]?.aav && (
+                    <span className="text-red-700">
+                      {errors?.players?.[index]?.aav?.message}
+                    </span>
+                  )}
+                </div>
+                <div className="w-full md:w-1/2 px-3 mb-2 md:mb-0">
+                  <label htmlFor="years" className={labelClass}>
+                    Years
+                  </label>
+                  <input
+                    id="years"
+                    key={field.id}
+                    className={inputClass}
+                    placeholder="1 - 3 years"
+                    {...register(`players.${index}.years`)}
+                  />
+                  {errors?.players?.[index]?.years && (
+                    <span className="text-red-700">
+                      {errors?.players?.[index]?.years?.message}
+                    </span>
+                  )}
+                </div>
+                {fields.length > 1 && (
+                  <button
+                    type="button"
+                    className="flex-shrink-0 border-transparent border-4 text-red-500 hover:text-red-800 text-sm py-1 px-2 rounded"
+                    onClick={() => remove(index)}
+                  >
+                    Remove player
+                  </button>
                 )}
               </div>
-              <div className="w-full md:w-1/2 px-3 mb-2 md:mb-0">
-                <label htmlFor="years" className={labelClass}>
-                  Years
-                </label>
-                <input
-                  id="years"
-                  key={field.id}
-                  className={inputClass}
-                  placeholder="1 - 3 years"
-                  {...register(`players.${index}.years`)}
-                />
-                {errors?.players?.[index]?.years && (
-                  <span className="text-red-700">
-                    {errors?.players?.[index]?.years?.message}
-                  </span>
-                )}
-              </div>
-              {fields.length > 1 && (
-                <button
-                  type="button"
-                  className="flex-shrink-0 border-transparent border-4 text-red-500 hover:text-red-800 text-sm py-1 px-2 rounded"
-                  onClick={() => remove(index)}
-                >
-                  Remove player
-                </button>
-              )}
             </div>
-          </div>
-        ))}
+          ))}
 
-        <div className="p-4">
-          <div className="mb-2">
-            Total salary:{" "}
-            <Cleave
-              value={salaryTotal}
-              options={{
-                numeral: true,
-                prefix: "$",
-                signBeforePrefix: true,
-                numeralThousandsGroupStyle: "thousand"
-              }}
-            />
+          <div className="p-4 text-center">
+            <button
+              type="button"
+              className={`${secondaryButtonClass}`}
+              onClick={() =>
+                append({
+                  name: "",
+                  aav: null,
+                  years: null
+                })
+              }
+            >
+              Add another player
+            </button>
           </div>
 
-          <button
-            type="button"
-            className={secondaryButtonClass}
-            onClick={() =>
-              append({
-                name: "",
-                aav: null,
-                years: null
-              })
-            }
-          >
-            Add another player
-          </button>
-          <input className={primaryButtonClass} type="submit" />
+          <div className="p-4">
+            <div className="mb-2 text-lg text-center">
+              <div className="text-green-800 font-bold">Total salary:</div>
+              <Cleave
+                value={salaryTotal}
+                className="text-center text-green-800"
+                options={{
+                  numeral: true,
+                  prefix: "$",
+                  signBeforePrefix: true,
+                  numeralThousandsGroupStyle: "thousand"
+                }}
+              />
+            </div>
+
+            {appState === "failed" ? (
+              <div>
+                <div className="text-red-700 mb-2">
+                  There was a serverside error upon submission. Please send the
+                  below error to the commissioners. You can also screenshot the
+                  page to manually submit your bids.
+                </div>
+                <code>
+                  <pre>{JSON.stringify(appErrors, null, 2)}</pre>
+                </code>
+              </div>
+            ) : (
+              <div className="p-4 text-center">
+                <input className={primaryButtonClass} type="submit" />
+              </div>
+            )}
+          </div>
+        </form>
+      )}
+
+      {appState === "success" && (
+        <div className="w-full mx-auto max-w-lg py-10">
+          <h1 className="text-3xl text-gray-800 text-center">
+            Thank you, your bids have been successfully submitted.
+          </h1>
         </div>
-      </form>
+      )}
     </div>
   );
 }
