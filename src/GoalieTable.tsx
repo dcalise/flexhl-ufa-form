b748a91baf416817a8dd07a6082e8eb2bce371df
Dan Calise <calise@gmail.com>
2022-07-03 11:41:44 -0400

    feat(tables): adding player/goalie tables

---
+++ src/GoalieTable.tsx
@@ -0,0 +1,87 @@
+import { goalieList } from "./data/batch1-goalie-table";
+import { thClasses, tdClasses, pClasses } from "./styles";
+
+export const GoalieTable = () => {
+  return (
+    <div className="overflow-x-auto">
+      <div className="inline-block min-w-full shadow rounded overflow-hidden">
+        <table className="table-auto min-w-full leading-normal overflow-scroll">
+          <thead>
+            <tr>
+              <th className={thClasses}>Goalie</th>
+              <th className={thClasses}>SK</th>
+              <th className={thClasses}>DU</th>
+              <th className={thClasses}>EN</th>
+              <th className={thClasses}>SZ</th>
+              <th className={thClasses}>AG</th>
+              <th className={thClasses}>RB</th>
+              <th className={thClasses}>SC</th>
+              <th className={thClasses}>HS</th>
+              <th className={thClasses}>RT</th>
+              <th className={thClasses}>PH</th>
+              <th className={thClasses}>PS</th>
+              <th className={thClasses}>EX</th>
+              <th className={thClasses}>LD</th>
+              <th className={thClasses}>OV</th>
+              <th className={thClasses}>Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            {goalieList.map((goalie) => (
+              <tr>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{goalie.name}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{goalie.SK}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{goalie.DU}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{goalie.EN}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{goalie.SZ}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{goalie.AG}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{goalie.RB}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{goalie.SC}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{goalie.HS}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{goalie.RT}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{goalie.PH}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{goalie.PS}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{goalie.EX}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{goalie.LD}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{goalie.OV}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{goalie.AGE}</p>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
---
+++ src/PlayerTable.tsx
@@ -0,0 +1,101 @@
+import { playerList } from "./data/batch1-player-table";
+import { thClasses, tdClasses, pClasses } from "./styles";
+
+export const PlayerTable = () => {
+  return (
+    <div className="overflow-x-auto">
+      <div className="inline-block min-w-full shadow rounded overflow-hidden">
+        <table className="table-auto min-w-full leading-normal overflow-scroll">
+          <thead>
+            <tr>
+              <th className={thClasses}>Skater</th>
+              <th className={thClasses}>Pos</th>
+              <th className={thClasses}>CK</th>
+              <th className={thClasses}>FG</th>
+              <th className={thClasses}>DI</th>
+              <th className={thClasses}>SK</th>
+              <th className={thClasses}>ST</th>
+              <th className={thClasses}>EN</th>
+              <th className={thClasses}>DU</th>
+              <th className={thClasses}>PH</th>
+              <th className={thClasses}>FO</th>
+              <th className={thClasses}>PA</th>
+              <th className={thClasses}>SC</th>
+              <th className={thClasses}>DF</th>
+              <th className={thClasses}>PS</th>
+              <th className={thClasses}>EX</th>
+              <th className={thClasses}>LD</th>
+              <th className={thClasses}>OV</th>
+              <th className={thClasses}>Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            {playerList.map((player) => (
+              <tr>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{player.name}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>
+                    {player.C || player.L || player.R ? "F" : "D"}
+                  </p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{player.CK}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{player.FG}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{player.DI}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{player.SK}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{player.ST}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{player.EN}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{player.DU}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{player.PH}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{player.FO}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{player.PA}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{player.SC}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{player.DF}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{player.PS}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{player.EX}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{player.LD}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{player.OV}</p>
+                </td>
+                <td className={tdClasses}>
+                  <p className={pClasses}>{player.age}</p>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
