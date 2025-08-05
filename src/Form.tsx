cead7aec30c90121cf810c47251dc6dcb16474d8
Dan Calise <calise@gmail.com>
2025-08-05 16:33:15 -0400

    feat: 25 batch 10

--- src/Form.tsx
+++ src/Form.tsx
@@ -16,8 +16,8 @@ import {
 
 import { PostgrestError } from "@supabase/supabase-js";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { playerList } from "./data/batch9-player-table";
-import { goalieList } from "./data/batch9-goalie-table";
+import { playerList } from "./data/batch10-player-table";
+import { goalieList } from "./data/batch10-goalie-table";
 
 import { schema } from "./schema";
 import { Inputs } from "./types";
@@ -70,7 +70,7 @@ export const Form = ({ appState, setAppState, isOpenBatch }: FormProps) => {
     }));
 
     const { data, error } = await supabase
-      .from("ufax2025_batch_9")
+      .from("ufax2025_batch_10")
       .insert(payload);
     if (error) {
       setAppErrors(error);
